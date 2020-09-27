2df17fa7de3b69575202945d8a7f238cac816b20
Parply <alex_pinches@btinternet.com>
2020-09-28 00:28:47 +0100

    tab

--- g-editor.cpp
+++ g-editor.cpp
@@ -18,6 +18,7 @@
 /*** defines ***/
 
 #define EDITOR_VERSION "0.0.1"
+#define G_TAB_STOP 8
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -37,12 +38,15 @@ enum editorKey {
 
 typedef struct erow {
   int size;
+  int rsize;
   char* chars;
+  char* render;
 } erow;
 
 struct editorConfig {
   int cx, cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -162,6 +166,26 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++)
+    if (row->chars[j] == '\t') tabs++;
+  free(row->render);
+  row->render = (char*)malloc(row->size + tabs*(G_TAB_STOP-1) + 1);
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % G_TAB_STOP != 0) row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
   E.row = (erow*) realloc(E.row, sizeof(erow) * (E.numrows + 1));
   
@@ -170,6 +194,11 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars = (char*) malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize=0;
+  E.row[at].render=NULL;
+  editorUpdateRow(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -224,6 +253,12 @@ void editorScroll(){
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
 
 void editorDrawRows(struct abuf *ab) {
@@ -234,7 +269,7 @@ void editorDrawRows(struct abuf *ab) {
     if (E.screenrows==0 && y == E.screenrows / 3) {
       char welcome[80];
       int welcomelen = snprintf(welcome, sizeof(welcome),
-        "e editor -- version %s", EDITOR_VERSION);
+        "g editor -- version %s", EDITOR_VERSION);
       if (welcomelen > E.screencols) welcomelen = E.screencols;
       int padding = (E.screencols - welcomelen) / 2;
       if (padding) {
@@ -247,9 +282,10 @@ void editorDrawRows(struct abuf *ab) {
       abAppend(ab, "~", 1);
     }
     } else {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].rsize - E.coloff;
+      if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[filerow].chars, len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);  
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -269,7 +305,7 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy-E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy-E.rowoff) + 1, (E.cx-E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -281,12 +317,21 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) E.cx--;
+      else if (E.cy > 0) {
+        E.cy--;
+        E.cx = E.row[E.cy].size;
+      }
       break;
     case ARROW_RIGHT:
-      if (E.cx != E.screencols-1) E.cx++;
+      if (row && E.cx < row->size) E.cx++;
+      else if (row && E.cx == row->size) {
+        E.cy++;
+        E.cx = 0;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) E.cy--;
@@ -294,6 +339,11 @@ void editorMoveCursor(int key) {
     case ARROW_DOWN:
       if (E.cy < E.numrows) E.cy++;
       break;
+  }
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
   }
 }
 
@@ -336,6 +386,7 @@ void initEditor() {
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
